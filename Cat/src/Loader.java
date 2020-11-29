dd94e72b9eab43327b8cbd31d836f17630348363
Vonadiks <vonadiks@mail.ru>
2020-11-30 00:11:35 +0300

    test1

---
+++ Cat/src/Loader.java
@@ -0,0 +1,25 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        //Cat cat = new Cat();
+
+        //System.out.println(cat.getStatus());
+
+        Cat vaska = new Cat();
+        //vaska.feed(5000.0);
+       /* int i = 0;
+        while (!vaska.getStatus().equals("Dead")){
+            vaska.meow();
+            i++;
+        }*/
+        vaska.feed(1000.0);
+
+        System.out.println(vaska.getFeedWeight());
+
+        System.out.println(vaska.getStatus());
+        //System.out.println("i = " + i);
+
+    }
+}
